92d74fd9a8d2ca999c51f0597dab9480614a3b53
Blayze <blayzestone@protonmail.com>
2020-07-27 15:48:31 -0700

    implement core game loop
    the core game loop can started/stopped with the 'play/pause' button.
    Currently the only event happening inside the core game loop is the
    generation iteration.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 function App() {
+  const [generation, setGeneration] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [runSpeed, setRunSpeed] = useState(1);
+
+  const runningRef = useRef(isRunning);
+  runningRef.current = isRunning;
+
+  const start = useCallback(
+    (nextGeneration) => {
+      if (!runningRef.current) {
+        return;
+      }
+
+      setGeneration(nextGeneration);
+
+      setTimeout(() => {
+        start(nextGeneration + 1);
+      }, 1000 / runSpeed);
+    },
+    [runSpeed]
+  );
+
+  useEffect(() => {
+    start(generation);
+  }, [isRunning, generation, start]);
+
   return (
     <>
       <h1>Conway's Game of Life</h1>
+      <button onClick={() => setIsRunning(!isRunning)}>
+        {!isRunning ? "Play" : "Pause"}
+      </button>
+      <h2>Generation #{generation}</h2>
     </>
   );
 }
