72e9630eedc8eeaa2bdc1e34b35eab172c97e933
Blayze <blayzestone@protonmail.com>
2020-07-29 22:43:29 -0700

    implement game of life ruleset

--- src/App.js
+++ src/App.js
@@ -20,12 +20,48 @@ function App() {
     return grid;
   }
 
+  function createNextGeneration(grid) {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const sum = getNeighborCount(i, j);
+
+        if (grid[i][j] && sum < 2) {
+          updateCellState(i, j);
+        }
+
+        if (!grid[i][j] && sum === 3) {
+          updateCellState(i, j);
+        }
+      }
+    }
+
+    return grid;
+  }
+
   function updateCellState(x, y) {
     const updatedGrid = [...grid];
 
     updatedGrid[x][y] = updatedGrid[x][y] ? 0 : 1;
 
     setGrid(updatedGrid);
+  }
+
+  function getNeighborCount(x, y) {
+    const relativeNeighborPositions = [
+      [x - 1, y],
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1],
+      [x + 1, y],
+      [x + 1, y - 1],
+      [x, y - 1],
+      [x - 1, y - 1],
+    ];
+
+    return relativeNeighborPositions.reduce((acc, [x, y]) => {
+      const cell = getCell(x, y);
+      return (acc += cell);
+    }, 0);
   }
 
   function getCell(x, y) {
@@ -44,6 +80,15 @@ function App() {
 
   return (
     <>
+      <button
+        onClick={() => {
+          let nextGrid = [...grid];
+          nextGrid = createNextGeneration(nextGrid);
+          setGrid(nextGrid);
+        }}
+      >
+        Play
+      </button>
       <Grid
         grid={grid}
         rows={rows}
