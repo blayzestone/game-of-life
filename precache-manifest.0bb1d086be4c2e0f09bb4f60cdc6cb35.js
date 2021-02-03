e8b9106f61b0591a6bbaf31bdb8fa70c44b69c99
Blayze <blayzestone@protonmail.com>
2021-02-03 00:16:59 -0800

    Updates

---
+++ precache-manifest.0bb1d086be4c2e0f09bb4f60cdc6cb35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe991de1ddf135f1eab5c2b16db6bf2",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "14913d64fe59c028e048",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4067c3bbdb0a8c8f3b94",
+    "url": "/game-of-life/static/js/main.8a2317ef.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
